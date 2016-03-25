7302923007a1017ca48d190db5afd53091285109
riswanda <root.devilscream@gmail.com>
2016-03-25 13:31:48 +0000

    Add library curl

---
+++ libs/http/curl.php
@@ -0,0 +1,25 @@
+<?php
+
+    function curl_post($url, $content, $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13") {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+    }
+    
+    function curl_get($url, $content, $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13"){
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL,$url.$content); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
+    $result = curl_exec($ch); 
+    curl_close($ch);
+    return $result;
+    }
+
+?>
