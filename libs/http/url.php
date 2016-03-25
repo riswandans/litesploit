42c2e98ba362257d05c2e0f001abe0a35b098c3e
riswanda <root.devilscream@gmail.com>
2016-03-25 16:10:49 +0000

    Add library url for send GET and POST

---
+++ libs/http/url.php
@@ -0,0 +1,30 @@
+<?php
+
+    function url_get($url)
+    {
+    $data = "";
+    $options = array(
+      'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'GET',
+        'content' => http_build_query($data),
+      ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    }
+
+    function url_post($url, $data)
+    {
+    $options = array(
+      'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+      ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    }
+    
+?>
