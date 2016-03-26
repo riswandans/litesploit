8fd525da6eeb9997fa9fb86e0e9767ad1a8c7875
riswanda <root.devilscream@gmail.com>
2016-03-26 12:51:29 +0000

    Fix library 'file'

--- libs/file.php
+++ libs/file.php
@@ -1,12 +1,12 @@
 <?php
-    function create($location, $content) {
+    function file_create($location, $content) {
     $dir = "/etc/litesploit/tmp/";
     $file = fopen($dir."/".$location,"w");
     fwrite($file, $content);
     fclose($file);
     }
     
-    function read($location) {
+    function file_ead($location) {
     $dir = "/etc/litesploit/tmp/";
     return file_get_contents($dir."/".$location, true);
     }
@@ -16,12 +16,12 @@
     return pathinfo($dir."/".$location, PATHINFO_EXTENSION);
     }
     
-    function time($format, $location) {
+    function file_time($format, $location) {
     $dir = "/etc/litesploit/tmp/";
     return date($format, filemtime($dir."/".$location));
     }
     
-    function copy($location, $newlocation) {
+    function file_copy($location, $newlocation) {
     $dir = "/etc/litesploit/tmp/";
     if (!copy($dir."/".$location, $dir."/".$newlocation)) {
     	echo "failed to copy...\n";
@@ -35,7 +35,7 @@
     }
     }
     
-    function delete($location) {
+    function file_delete($location) {
     $dir = "/etc/litesploit/tmp/";
     if (!unlink($dir."/".$location)) {
     	echo "failed to delete...\n";
