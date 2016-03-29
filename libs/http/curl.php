b3a79ac5743f10e525e618af5d66c7311adbf2f2
riswanda <root.devilscream@gmail.com>
2016-03-29 10:02:20 +0000

    Add CURLOPT_HEADER at library curl

--- libs/http/curl.php
+++ libs/http/curl.php
@@ -3,6 +3,7 @@
     function curl_post($url, $content, $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13") {
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_HEADER, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -14,6 +15,7 @@
     
     function curl_get($url, $content, $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13"){
     $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_HEADER, true);
     curl_setopt($ch, CURLOPT_URL,$url."/".$content); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
@@ -25,10 +27,11 @@
     function curl_download($url, $savelocation){
     $savelocation = fopen ($savelocation, 'w+');
     $ch = curl_init(str_replace(" ","%20",$location));
+    curl_setopt($ch, CURLOPT_HEADER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 50);
     curl_setopt($ch, CURLOPT_FILE, $savelocation);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    $result = curl_exec($ch);//get curl response
+    $result = curl_exec($ch);
     curl_close($ch);
     return $result;
     }
@@ -36,6 +39,7 @@
     function curl_upload($url, $uploadfile){
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HEADER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS,
          array('Filedata'=>"@$uploadfile"));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
