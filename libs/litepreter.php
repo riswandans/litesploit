58a5d7856152c6a2db5a0fe8fee21b9f7a1f18d2
riswanda <root.devilscream@gmail.com>
2016-04-07 12:24:42 +0000

    Fix library litepreter

--- libs/litepreter.php
+++ libs/litepreter.php
@@ -4,7 +4,7 @@
     echo "[*] Litepreter running\n";
     echo "[*] Running on $ip:$port \n";
     echo "[*] Type Ctrl+C for stop\n";
-    exec("cd $location && php -S $ip:$port /etc/litesploit/app/litepreter");
+    exec("php -S $ip:$port $location");
     }
 
 ?>
