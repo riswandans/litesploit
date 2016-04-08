e9ef8392dad7122b5d92f16eb2f1acf1e60b6be8
riswanda <root.devilscream@gmail.com>
2016-04-08 06:00:21 +0000

    Fix library curl

--- libs/http/curl.php
+++ libs/http/curl.php
@@ -17,12 +17,10 @@
     function curl_get($url, $content){
     $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_URL,$url."/".$content); 
-    curl_setopt($ch, CURLOPT_HEADER, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
-    curl_setopt($ch, CURLOPT_VERBOSE, true);
     $result = curl_exec($ch); 
     curl_close($ch);
     return $result;
